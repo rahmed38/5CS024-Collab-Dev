582f6c56f59d0354b4048c65ebfb541030cac5b6
rahmed38 <r.ahmed38@wlv.ac.uk>
2025-03-15 05:16:16 +0000

    Add files via upload

--- Database DDL.sql
+++ Database DDL.sql
@@ -2,37 +2,35 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
-
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8;
+USE `mydb`;
 
 -- Table `mydb`.`User`
-
 CREATE TABLE IF NOT EXISTS `mydb`.`User` (
   `user_id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(50) NOT NULL,
-  `password` VARBINARY(255) NOT NULL, --Changed the VARCHAR to VARBINARY to store encrypted data--
-  `arts` TINYINT NOT NULL DEFAULT 0,
-  `business` TINYINT NOT NULL DEFAULT 0,
-  `engineering` TINYINT NOT NULL DEFAULT 0,
-  `health` TINYINT NOT NULL DEFAULT 0,
-  `law` TINYINT NOT NULL DEFAULT 0,
-  `science` TINYINT NOT NULL DEFAULT 0,
-  `dance` TINYINT NOT NULL DEFAULT 0,
-  `computer_science` TINYINT NOT NULL DEFAULT 0,
-  `psychology` TINYINT NOT NULL DEFAULT 0,
-  `marketing` TINYINT NOT NULL DEFAULT 0,
+  `password` VARBINARY(255) NOT NULL, -- Changed to VARBINARY to store encrypted data --
+  `arts` BOOLEAN NOT NULL DEFAULT FALSE,
+  `business` BOOLEAN NOT NULL DEFAULT FALSE,
+  `engineering` BOOLEAN NOT NULL DEFAULT FALSE,
+  `health` BOOLEAN NOT NULL DEFAULT FALSE,
+  `law` BOOLEAN NOT NULL DEFAULT FALSE,
+  `science` BOOLEAN NOT NULL DEFAULT FALSE,
+  `dance` BOOLEAN NOT NULL DEFAULT FALSE,
+  `computer_science` BOOLEAN NOT NULL DEFAULT FALSE,
+  `psychology` BOOLEAN NOT NULL DEFAULT FALSE,
+  `marketing` BOOLEAN NOT NULL DEFAULT FALSE,
   PRIMARY KEY (`user_id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE
 ENGINE = InnoDB;
 
 -- Ruqaiyyah 2336189 Encryption code --
 INSERT INTO `mydb`.`User`(`username`,`password`)
 VALUES ('encrypt_user',AES_ENCRYPT('encrypt_password','secret_key'));
+
 SELECT `username`, AES_DECRYPT(`password`, 'secret_key') AS `password`
 FROM `mydb`.`User`;
--- Ruqaiyyah 2336189 Encryption code -- 
-
+-- Ruqaiyyah 2336189 Encryption code --
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
